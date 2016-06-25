dc1efe1e96021f89754403e62ce7a3e7a6fa3705
Baiquan Fan <baiquan.fan@gmail.com>
2016-06-26 00:39:17 +0300

    Adds pagination in product model with kaminari.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,30 @@
 class Product < ActiveRecord::Base
+	paginates_per 25
+
 	belongs_to :user
 
   validates_presence_of :name, :price, :stock, :user_id
   validates_numericality_of :price, greater_than_or_equal_to: 0
   validates_numericality_of :stock, greater_than_or_equal_to: 0
+
+	scope :filter_by_name, lambda { |keyword|
+    where("lower(name) LIKE ?", "%#{keyword.downcase}%" ) 
+ 	}
+
+  scope :above_or_equal_to_price, lambda { |price| 
+    where("price >= ?", price) 
+  }
+
+  scope :below_or_equal_to_price, lambda { |price| 
+    where("price <= ?", price) 
+  }
+
+  scope :order_by_name, -> {
+    order(:name)
+  }
+
+  scope :order_by_price, -> {
+    order(:price)
+  }
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -21,4 +21,91 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of :user_id }
 
   it { is_expected.to belong_to :user }
+
+  describe ".filter_by_name" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, name: "A plasma TV"
+      @product2 = FactoryGirl.create :product, name: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, name: "CD player"
+      @product4 = FactoryGirl.create :product, name: "LCD TV"
+    end
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_name("TV").length).to eq(2)
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_name("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+    end
+  end
+
+  describe ".below_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+    end
+  end
+
+  describe ".order_by_name" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, name: "A plasma TV"
+      @product2 = FactoryGirl.create :product, name: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, name: "CD player"
+      @product4 = FactoryGirl.create :product, name: "LCD TV"
+
+      # Touch some products to update them
+      @product2.touch
+      @product3.touch
+    end
+
+    it "returns the records ordered by name" do
+      expect(Product.order_by_name).to match_array([@product1, @product3, @product2, @product4])
+    end
+  end
+
+  describe ".order_by_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, name: "A plasma TV"
+      @product2 = FactoryGirl.create :product, name: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, name: "CD player"
+      @product4 = FactoryGirl.create :product, name: "LCD TV"
+    end
+
+    it "returns the records ordered by price" do
+      expect(Product.order_by_name).to match_array([@product2, @product4, @product1, @product3])
+    end
+  end
+
+  describe ".page" do
+    before(:each) do
+      58.times { FactoryGirl.create :product }
+    end
+
+    it "returns the records 5 records per page" do
+      expect(Product.page(1).count).to eq(25)
+      expect(Product.page(2).count).to eq(25)
+      expect(Product.page(3).count).to eq(8)
+    end
+  end
 end
