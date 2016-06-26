d8da3d25ed6ba378e845d7acc4a8239f54de961c
Baiquan Fan <baiquan.fan@gmail.com>
2016-06-26 22:05:41 +0300

    Adds pagination for the products index action to optimize response.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	paginates_per 25
+#	paginates_per 25
 
 	belongs_to :user
   has_many :order_items
@@ -30,14 +30,14 @@ class Product < ActiveRecord::Base
   }
 
   def self.search(params = {})
-    products = params[:product_ids].present? ? Product.find(params[:product_ids]) : Product.all
+    products = params[:product_ids].present? ? Product.where(id: params[:product_ids]) : Product.all
 
     products = products.filter_by_name(params[:keyword]) if params[:keyword]
     products = products.above_or_equal_to_price(params[:min_price].to_f) if params[:min_price]
     products = products.below_or_equal_to_price(params[:max_price].to_f) if params[:max_price]
     products = products.order_by_name if params[:order_by_name].present?
     products = products.order_by_price if params[:order_by_price].present?
-    products = products.page(params[:page]) if params[:page].present?
+    #products = products.page(params[:page]) if params[:page].present?
 
     products
   end
