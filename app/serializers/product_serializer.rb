195abab3461ea93d2c1abf1edd2c531d53ea4347
Baiquan Fan <baiquan.fan@gmail.com>
2016-06-26 22:32:02 +0300

    Adds caching for the serializers.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,11 @@
 class ProductSerializer < ActiveModel::Serializer
+  cached
+
   attributes :id, :name, :price, :stock, :published
 
   has_one :user
+
+  def cache_key
+    [object, scope]
+  end
 end
